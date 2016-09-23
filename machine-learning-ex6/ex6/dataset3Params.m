ab3f177734947666998ef9f46d936ed8f771d8f5
Matias Iordache <matias@acid.cl>
2016-09-23 12:16:47 -0300

    finished exercise 6 svm and spam classifier

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -24,8 +24,20 @@ sigma = 0.3;
 %
 
 
-
-
+%predictions = svmPredict(model, Xval);
+bestPrediction = 1000;
+for C_i = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+  for sigma_i = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+    model= svmTrain(X, y, C_i, @(x1, x2) gaussianKernel(x1, x2, sigma_i));
+    predictions = svmPredict(model, Xval);
+    prediction = mean(double(predictions ~= yval));
+    if prediction < bestPrediction;
+      bestPrediction = prediction;
+      C = C_i;
+      sigma = sigma_i;
+    end
+  end
+end
 
 
 
